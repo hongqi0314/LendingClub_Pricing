78ab3be6c4da6a4fafc71d0f50babb453129509c
hongqi0314 <la840829@gmail.com>
2020-08-18 09:24:44 -0400

    Calculation of debt and re-calculation of overall DTI.

--- load_data_and_clean.R
+++ load_data_and_clean.R
@@ -1,7 +1,6 @@
 ## Load data and initial cleanup
 
 #### 0. Load required packages ----
-devtools::install_local(path = "E:/RProjects/ALF_RPackage/utilALF")
 utilALF::load_pkgs()
 
 #### 1. Read data from the .csv file ----
@@ -12,6 +11,24 @@ lc_data_2007_to_2018Q4 <-
   )
 
 lc_data <- lc_data_2007_to_2018Q4 %>%
-  filter(!is.na(id))
+  dplyr::filter(!is.na(id))
 
 stopifnot(nrow(lc_data) == length(unique(lc_data$id)))
+
+#### 2. Data cleaning and feature selection ----
+### 2.1 Re-calculate income, debt, and DTI ----
+lc_temp_debtCalc <- lc_data %>% 
+  dplyr::filter(
+      !(is.na(annual_inc) & is.na(annual_inc_joint))
+    , dti >= 0.00 # exclude records with negative DTI.
+    , dti < 999.00 # exclude records with DTI 999.00 because it looks like a truncated value.
+  ) %>% 
+  dplyr::mutate(
+    annual_inc_total = annual_inc + 
+      ifelse(is.na(annual_inc_joint), 0.00, annual_inc_joint),
+    debt_total = annual_inc * (dti / 100) +
+      ifelse(is.na(annual_inc_joint) | is.na(dti_joint), 0.00, 
+             annual_inc_joint * (dti / 100)), 
+    dti_total = round(debt_total / annual_inc_total * 100, 2)
+  )
+  
