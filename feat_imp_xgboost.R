52d5f5568e92ecf8015c14a577ae1795ef1ae427
hongqi0314 <la840829@gmail.com>
2020-09-05 22:32:23 -0400

    xgboost 'max_depth' manual tuning added.

--- feat_imp_xgboost.R
+++ feat_imp_xgboost.R
@@ -47,3 +47,62 @@ lc_xgb_cv <- xgb.cv(
   , prediction = TRUE
   , metrics = 'auc'
 )
+
+#### 1. Manual hyper-parameter tuning ----
+### 1.1 "max_depth" ----
+scores_max_depth <- c()
+
+for (max_depth in c(3, 4, 5, 6)) {
+  
+  lc_param <- list(
+      'objective' = 'binary:logistic'
+    , 'eta' = 0.01
+    , 'max_depth' = max_depth
+    , 'min_child_weight' = 1
+    , 'colsample_bytree' = 1
+    , 'subsample' = 1
+    , 'gamma' = 0
+  )
+  
+  cv_results <- xgb.cv(
+      params = lc_param 
+    , data = lc_xgb_train_sample[, xgb_colnames != "loan_status"]
+    , label = lc_xgb_train_sample[, "loan_status"]
+    , nfold = 5
+    , nrounds = 2500
+    , early_stopping_rounds = 500
+    , verbose = TRUE
+    , stratified = TRUE
+    , prediction = TRUE
+    , metrics = 'auc'
+    , seed = 12345
+  )
+  
+  best_iteration <- cv_results$best_iteration
+  best_score <- max(cv_results$evaluation_log[['test_auc_mean']])
+  print(glue::glue("'max_depth': {max_depth} -- 
+                    'best_iteration': {best_iteration} --
+                    'best_score': {best_score}"))
+  scores_max_depth <- 
+    c(scores_max_depth, 
+      best_score, 
+      lc_param[['eta']], 
+      lc_param[['max_depth']],
+      lc_param[['min_child_weight']], 
+      lc_param[['colsample_bytree']],
+      lc_param[['subsample']], 
+      lc_param[['gamma']], 
+      best_iteration)
+  
+}
+
+scores_max_depth_display <- 
+  as.data.frame( matrix(scores_max_depth, ncol = 8, byrow = TRUE) )
+names(scores_max_depth_display) <- 
+  c('score', 'eta', 'max_depth', 'min_child_weight', 'colsample_bytree', 
+    'subsample', 'gamma', 'best_iteration')
+best_max_depth <- scores_max_depth_display %>%
+  dplyr::top_n(1, score) %>%
+  dplyr::pull(max_depth)
+
+print(glue::glue("best 'max_depth' is {best_max_depth}."))
